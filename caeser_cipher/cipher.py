a61aacbbf090d9baeb0c3e05451e951a638ea077
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-07-05 15:07:20 -0700

    file structure

---
+++ caeser_cipher/cipher.py
@@ -0,0 +1,16 @@
+from caeser_cipher.corpus_loader import word_list, name_list
+
+def encrypt(plain_text, shift):
+      encrypted_text = ""
+      for char in plain_text:
+            if char.isupper():
+
+def decrypt(encrypted_test, shift):
+      pass
+
+def crack(encrypted_test):
+      pass
+
+
+if __name__ == "__main__":
+      print("cypher.py is showing")
---
+++ caeser_cipher/is_english_word.py
@@ -0,0 +1,16 @@
+from corpus_loader import word_list, name_list
+
+# print(name_list)
+
+
+# sentence: 'The quick brown fox jumped over the lazily sleeping dog'
+def find_word(sentence):
+    for word in sentence.split():
+        if word in word_list:
+            print(f'{word} It is here')
+        else:
+            print(f'{word} Nope,not here')
+
+
+if __name__ == '__main__':
+    find_word('IWT FJXRZ QGDLC UDM YJBETS DKTG IWT APOXAN HATTEXCV SDV')
